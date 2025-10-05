ed68848171681a0c16b79afdbf12d191d9a38761
Victorvictor8 <onalovictor8@gmail.com>
2025-10-05 23:03:13 +0100

    Add files via upload

---
+++ new react app/index.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello, Victor!</h1>
+      <p>This React app is connected to the div named “Victor”.</p>
+    </div>
+  );
+}
+
+export default App;
